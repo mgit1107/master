96c63141eb0f8116673ef8d1d563400b96a72684
maosq <244956519@qq.com>
2020-05-22 15:39:14 +0800

    这是在新的分支上加的

--- untitled/src/main/java/cn/com/taiji/TestBranch.java
+++ untitled/src/main/java/cn/com/taiji/TestBranch.java
@@ -1,7 +1,12 @@
 package cn.com.taiji;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TestBranch {
     public static void main(String[] args) {
         System.out.println("新的分支");
+        Map<String,Integer> map = new HashMap<>();
     }
 }
